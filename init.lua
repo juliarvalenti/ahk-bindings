2df1da34a62893882355eb0af434381af1014be4
Julia Valenti <julvalen@cisco.com>
2021-03-23 18:23:59 -0400

    switching cmd -> ctrl

--- init.lua
+++ init.lua
@@ -19,108 +19,109 @@ end
 
 local fastKeyStroke = function(modifiers, character)
     local event = require("hs.eventtap").event
+    print(character)
     event.newKeyEvent(modifiers, string.lower(character), true):post()
     event.newKeyEvent(modifiers, string.lower(character), false):post()
 end
 
 local keys = {{
-    sMod = {"cmd"},
+    sMod = {"ctrl"},
     sKey = "J",
     dMod = {},
     dKey = "Left",
     description = "Simple movement left"
 }, {
-    sMod = {"cmd"},
+    sMod = {"ctrl"},
     sKey = "K",
     dMod = {},
     dKey = "Down",
     description = "Simple movement down"
 }, {
-    sMod = {"cmd"},
+    sMod = {"ctrl"},
     sKey = "I",
     dMod = {},
     dKey = "Up",
     description = "Simple movement up"
 }, {
-    sMod = {"cmd"},
+    sMod = {"ctrl"},
     sKey = "L",
     dMod = {},
     dKey = "Right",
     description = "Simple movement right"
 }, {
-    sMod = {"cmd", "shift", "ctrl"},
+    sMod = {"ctrl", "shift", "cmd"},
     sKey = "J",
     dMod = {"alt", "shift"},
     dKey = "Left",
     description = "Highlight words left"
 }, {
-    sMod = {"cmd", "shift", "ctrl"},
+    sMod = {"ctrl", "shift", "cmd"},
     sKey = "K",
     dMod = {"shift"},
     dKey = "Down",
     description = "Highlight words down"
 }, {
-    sMod = {"cmd", "shift", "ctrl"},
+    sMod = {"ctrl", "shift", "cmd"},
     sKey = "I",
     dMod = {"shift"},
     dKey = "Up",
     description = "Highlight words up"
 }, {
-    sMod = {"cmd", "shift", "ctrl"},
+    sMod = {"ctrl", "shift", "cmd"},
     sKey = "L",
     dMod = {"alt", "shift"},
     dKey = "Right",
     description = "Highlight words right"
 }, {
-    sMod = {"cmd", "shift"},
+    sMod = {"ctrl", "shift"},
     sKey = "I",
     dMod = {"cmd"},
     dKey = "Up",
-    description = "ctrl home, go to top of page"
+    description = "cmd home, go to top of page"
 }, {
-    sMod = {"cmd", "shift"},
+    sMod = {"ctrl", "shift"},
     sKey = "K",
     dMod = {"cmd"},
     dKey = "Down",
-    description = "ctrl end, go to bottom of page"
+    description = "cmd end, go to bottom of page"
 }, {
-    sMod = {"cmd", "ctrl"},
+    sMod = {"ctrl", "cmd"},
     sKey = "L",
     dMod = {"alt"},
     dKey = "Right",
     description = "traverse right by one word"
 }, {
-    sMod = {"cmd", "ctrl"},
+    sMod = {"ctrl", "cmd"},
     sKey = "J",
     dMod = {"alt"},
     dKey = "Left",
     description = "traverse left by one word"
 }, {
-    sMod = {"cmd", "shift"},
+    sMod = {"ctrl", "shift"},
     sKey = "L",
     dMod = {"cmd"},
     dKey = "Right",
     description = "end key, go to end of line"
 }, {
-    sMod = {"cmd", "shift"},
+    sMod = {"ctrl", "shift"},
     sKey = "J",
     dMod = {"cmd"},
     dKey = "Left",
     description = "home key, go to front of line"
 }, {
-    sMod = {"cmd", "ctrl"},
+    sMod = {"ctrl", "cmd"},
     sKey = "I",
     dMod = {"fn"},
     dKey = "Up",
     description = "Page up"
 }, {
-    sMod = {"cmd", "ctrl"},
+    sMod = {"ctrl", "cmd"},
     sKey = "K",
     dMod = {"fn"},
     dKey = "Down",
     description = "Page down"
 }, {
-    sMod = {"ctrl"},
+    sMod = {"cmd"},
     sKey = "Delete",
     dMod = {"alt"},
     dKey = "Delete",
